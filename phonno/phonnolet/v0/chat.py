f264c60fb3a4d1ef04be27328a0b6ca44f358dfa
daiiz <da.iz.mg@gmail.com>
2023-07-24 08:06:07 +0900

    d

--- phonno/phonnolet/v0/chat.py
+++ phonno/phonnolet/v0/chat.py
@@ -1,2 +1,18 @@
+import requests
+
+
 def similar_with(queries, topk=5, origin="", token=""):
-    print("queries", queries)
+    qs = []
+    for q in queries:
+        if isinstance(q, list):
+            continue
+        else:
+            qs.append(q.strip())
+    if len(qs) == 0:
+        return []
+    api_url = "{}/api/chat/similar?q={}&limit={}".format(
+        origin,
+        " ".join(qs),
+        topk,
+    )
+    print(api_url)
