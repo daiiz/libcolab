b58d0eec08e3051ede5623864aa6ca972287c30a
daiiz <da.iz.mg@gmail.com>
2023-07-23 15:15:03 +0900

    get_token

--- phonno/app.py
+++ phonno/app.py
@@ -9,3 +9,12 @@ def run_command(command):
 def sample5(msg):
     print(msg)
     print(run_command("ls -l"))
+
+
+def get_token(drive_root_name, colab_name):
+    base_dir = "/content/drive/MyDrive/{0}/{1}".format(drive_root_name, colab_name)
+    key_file_path = "{0}/cred.json".format(base_dir)
+    aud = ""
+    with open("{0}/client_id.txt".format(base_dir), "r") as fp:
+        aud = fp.readline()
+    print("...", aud)
