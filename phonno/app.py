f6235aa3036003fdb3e9794c94a0114de0de2721
daiiz <da.iz.mg@gmail.com>
2023-07-23 15:11:11 +0900

    5

--- phonno/app.py
+++ phonno/app.py
@@ -1,10 +1,15 @@
-import os
+import subprocess
+
+
+def run_command(command):
+    completed_process = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    return completed_process.stdout.decode("utf-8")  # return output as string
 
 
 def run_command(command):
     return os.system(command)
 
 
-def sample4(msg):
+def sample5(msg):
     print(msg)
     print(run_command("ls -l"))
