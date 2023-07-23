934b649c6b115a1f39b840477cfe57df3d5c65cb
daiiz <da.iz.mg@gmail.com>
2023-07-23 21:19:44 +0900

    b

--- phonno/phonnolet/v0/render.py
+++ phonno/phonnolet/v0/render.py
@@ -146,7 +146,7 @@ def get_anno_anchor_content_image_background_style(type, img_url):
                 "background-size": "cover",
                 "background-position": "center center",
                 "background-repeat": "no-repeat",
-                "background-image": 'url("{}")'.format(img_url),
+                "background-image": "url('{}')".format(img_url),
             }
         )
     return ""
