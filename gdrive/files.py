0ec8a508f3969338a906787d6f335248b29bb4ae
daiiz <da.iz.mg@gmail.com>
2023-07-23 14:54:03 +0900

    impl

--- gdrive/files.py
+++ gdrive/files.py
@@ -1,2 +1,6 @@
-def load_single_text(msg):
-  print(msg)
+def load_single_text(file_path, root_dir_name="MyDrive"):
+    if not file_path:
+        raise ValueError("file_path is required")
+    with open("/content/drive/{}/{}".format(root_dir_name, file_path), "r") as fp:
+        line = fp.readlines()[0].strip()
+        return line
