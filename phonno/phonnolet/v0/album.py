41ecf472f20dbb75fb3b1f938cfed95f40379d02
daiiz <da.iz.mg@gmail.com>
2023-09-05 01:21:59 +0900

    impl get_photos_at

--- phonno/phonnolet/v0/album.py
+++ phonno/phonnolet/v0/album.py
@@ -16,4 +16,24 @@ def _convertToMilliSeconds(date_str, zone="UTC"):
     return int(timestamp * 1000)
 
 
-# def get_photos_at()
+def get_photos_at(date_str, location, origin="", token="", zone="UTC"):
+    if not date_str and not location:
+        raise Exception("No date or location provided")
+    api_url = "{}/api/memory/photos".format(origin)
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer " + token,
+    }
+    payload = {}
+    if date_str:
+        payload["timestamp"] = _convertToMilliSeconds(date_str, zone)
+    if location:
+        payload["location"] = [location["lng"], location["lat"]]
+
+    r = requests.post(api_url, json=payload, headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        return data["images"]
+    else:
+        raise Exception("Error: {}".format(r.status_code))
