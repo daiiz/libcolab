e4261b89fa34f1f73b02c5383ec16d9e56d37a8c
Daiki Iizuka <da.iz.mg@gmail.com>
2024-05-06 09:39:09 +0900

    Phonnolet: render.show_chat関数にthemeを渡せるようにする  (#14)
    * show_chatにthemeを渡せる
    
    * show_annotationsにthemeを引き継ぐ
    
    * anno URLのパターンにもproject_nameを適用

--- phonno/phonnolet/v0/render.py
+++ phonno/phonnolet/v0/render.py
@@ -205,7 +205,7 @@ def show_annotations(
                 url = "{}/{}/{}".format(origin, project_name, image_id)
                 img_url = "{}/{}/thumb/300".format(img_origin, image_id)
             else:
-                url = "{}/{}#a{}".format(origin, image_id, int(anno_id) + 1)
+                url = "{}/{}/{}#a{}".format(origin, project_name, image_id, int(anno_id) + 1)
                 img_url = "{}/api/data/annotations_images?imageId={}&annoId={}".format(
                     origin, image_id, str(anno_id)
                 )
@@ -264,7 +264,7 @@ def show_images(image_ids, origin="", project_name="", repeat_in_row=6, indent=F
     )
 
 
-def show_chat(data, origin="", project_name="", q=False, a=False):
+def show_chat(data, origin="", project_name="", q=False, a=False, theme=""):
     chat_id = data["chatId"]
     chat_uri = "{}/share/{}".format(origin, chat_id)
     IPython.display.display(
@@ -279,7 +279,7 @@ def show_chat(data, origin="", project_name="", q=False, a=False):
                 query.append([item["imageId"], int(item["annoId"])])
             elif item["type"] == "text":
                 query.append(item["text"])
-        show_annotations(query, origin=origin, project_name=project_name, style="query", indent=True)
+        show_annotations(query, origin=origin, project_name=project_name, style="query", indent=True, theme=theme)
     if a:
         html_lines = []
         html_lines.append(
@@ -296,7 +296,7 @@ def show_chat(data, origin="", project_name="", q=False, a=False):
         for annoKey in data["hitDocs"].keys():
             anno = data["hitDocs"][annoKey]
             annos.append([anno["imageId"], int(anno["annoId"])])
-        show_annotations(annos, origin=origin, project_name=project_name, style="result", indent=True)
+        show_annotations(annos, origin=origin, project_name=project_name, style="result", indent=True, theme=theme)
 
     IPython.display.display(
         IPython.display.HTML(
